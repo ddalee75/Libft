b4110bcb7fd37b2567d3f922d83fb4827a58f368
Ching-lin LEE <chilee@student.42.fr>
2021-12-06 18:38:12 +0100

    06/12/21

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,37 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chilee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/01 17:27:00 by chilee            #+#    #+#             */
-/*   Updated: 2021/12/06 11:05:00 by chilee           ###   ########.fr       */
+/*   Created: 2021/12/03 10:32:34 by chilee            #+#    #+#             */
+/*   Updated: 2021/12/06 11:21:30 by chilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*dest;
-	size_t	i;
+	int		i;
+	int		j;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	dest = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (dest == NULL)
+		return (NULL);
 	i = 0;
-	if (s == NULL)
-		return (NULL);
-	if (ft_strlen(s) < start)
-		len = 0;
-	if (len > ft_strlen(s) - start)
-		len = ft_strlen(s) - start;
-	dest = (char *)malloc(sizeof(char) * (len + 1));
-	if (dest == 0)
-		return (NULL);
-	while (i < len)
+	while (s1[i])
 	{
-		dest[i] = s[start + i];
+		dest[i] = s1[i];
 		i++;
 	}
-	dest[i] = '\0';
+	j = 0;
+	while (s2[j])
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
 	return (dest);
 }
